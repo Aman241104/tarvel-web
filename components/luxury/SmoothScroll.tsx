9aca211658287c9b7363fd810aefc97ae59f0655
Aman241104 <aman@example.com>
2026-02-16 04:33:57 +0530

    fix: update lenis import and remove deprecated options for Vercel build

--- components/luxury/SmoothScroll.tsx
+++ components/luxury/SmoothScroll.tsx
@@ -1,19 +1,15 @@
 "use client";
 
 import { useEffect } from "react";
-import Lenis from "@studio-freight/lenis";
+import Lenis from "lenis";
 
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
     useEffect(() => {
         const lenis = new Lenis({
             duration: 1.2,
-            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
-            direction: "vertical",
-            gestureDirection: "vertical",
-            smooth: true,
-            mouseMultiplier: 1,
-            smoothTouch: false,
-            touchMultiplier: 2,
+            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+            orientation: "vertical",
+            wheelMultiplier: 1,
         });
 
         function raf(time: number) {
